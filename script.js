53d71c8b800a8e8dc9b45b3870a7c0e440a931d1
Billionaire-coder <152015213+Billionaire-coder@users.noreply.github.com>
2023-11-30 16:41:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+alert("Please on the Desktop site option from chrome setting and rotate the phone for better website-view experience!");
+
+
 const scroller = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true
